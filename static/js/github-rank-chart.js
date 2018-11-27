152d50d8a4eab31ae7881bfcac3c4bc40526b46b
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-11-27 05:30:33 -0500

    Update Website Content

--- static/js/github-rank-chart.js
+++ static/js/github-rank-chart.js
@@ -3,7 +3,7 @@ var axFontSize = 17;
 var myChart = new Chart(ctx, {
 	type: 'bar',
 	data: {
-		labels: ['TensorFlow', 'Django', 'Flask', 'Atom', 'pytest', 'qutebrowser', 'xmonad', 'cookiecutter', 'khal'],
+		labels: ['TensorFlow', 'Django', 'Flask', 'Atom', 'Pytest', 'Qutebrowser', 'XMonad', 'Cookiecutter', 'Khal'],
 		datasets: [{
 			label: '',
 			data: [5.00, 4.59, 3.74, 3.33, 3.30, 2.49, 2.47, 2.26, 2.12],
