0da2f0cededb3a66d27cc60c26e872bb7083324b
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-11-27 05:06:42 -0500

    Update Website Content

--- static/js/github-rank-chart.js
+++ static/js/github-rank-chart.js
@@ -6,15 +6,6 @@ var myChart = new Chart(ctx, {
 		datasets: [{
 			label: '# of Votes',
 			data: [5.00, 4.59, 3.74, 3.33, 3.30, 2.49, 2.47, 2.26, 2.12],
-			backgroundColor: coloR,
-			borderColor: [
-'rgba(255,99,132,1)',
-'rgba(54, 162, 235, 1)',
-'rgba(255, 206, 86, 1)',
-'rgba(75, 192, 192, 1)',
-'rgba(153, 102, 255, 1)',
-'rgba(255, 159, 64, 1)'
-			],
 			borderWidth: 1
 		}]
 	},
