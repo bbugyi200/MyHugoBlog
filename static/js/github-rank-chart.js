2c63f015e9d7a131092f834d1e5f1e607aec5e73
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-11-27 06:13:09 -0500

    Update Website Content

--- static/js/github-rank-chart.js
+++ static/js/github-rank-chart.js
@@ -3,19 +3,19 @@ var axFontSize = 17;
 var myChart = new Chart(ctx, {
 	type: 'bar',
 	data: {
+        dataPoints: [
+            { label: "TensorFlow", y: 5.00, link: "" },
+            { label: "Django", y: 4.59, link: "" },
+            { label: "Flask", y: 3.74, link: "" },
+            { label: "Atom", y: 3.33, link: "" },
+            { label: "Pytest", y: 3.30, link: "" },
+            { label: "Qutebrowser", y: 2.49, link: "" },
+            { label: "XMonad", y: 2.47, link: "" },
+            { label: "Cookiecutter", y: 2.26, link: "" },
+            { label: "Khal", y: 2.12, link: "" }
+        ],
 		datasets: [{
 			label: '',
-			dataPoints: [
-                { label: "TensorFlow", y: 5.00, link: "" },
-                { label: "Django", y: 4.59, link: "" },
-                { label: "Flask", y: 3.74, link: "" },
-                { label: "Atom", y: 3.33, link: "" },
-                { label: "Pytest", y: 3.30, link: "" },
-                { label: "Qutebrowser", y: 2.49, link: "" },
-                { label: "XMonad", y: 2.47, link: "" },
-                { label: "Cookiecutter", y: 2.26, link: "" },
-                { label: "Khal", y: 2.12, link: "" }
-            ],
 			backgroundColor: [
 'rgba(255, 99, 132, 0.2)',
 'rgba(54, 162, 235, 0.2)',
